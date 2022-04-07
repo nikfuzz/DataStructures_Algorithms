6158706fe5640477fd2c495bd475b233f0653758
Nikhil <nik.dhoot@gmail.com>
2022-04-07 08:47:19 +0530

    last stone weight

---
+++ leetcode/heaps/1046_Last_Stone_Weight.py
@@ -0,0 +1,62 @@
+'''
+1046. Last Stone Weight
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+If x == y, both stones are destroyed, and
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+
+Return the smallest possible weight of the left stone. If there are no stones left, return 0.
+
+ 
+
+Example 1:
+
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+Example 2:
+
+Input: stones = [1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+'''
+# make a min heap with negetives or make a max heap
+# pop two elements at a time and push the diff if the diff is not 0
+# keep running the above step until the heap size reaches below or equal to 1
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        h = []
+        
+        for i in range(len(stones)):
+            heapq.heappush(h, -stones[i])
+        
+        while len(h) > 1:
+            stone1 = -heapq.heappop(h)
+            stone2 = -heapq.heappop(h)
+            
+            if stone1 != stone2:
+                heapq.heappush(h, -abs(stone1-stone2)) 
+            
+        if len(h) == 0:
+            return 0
+        else:
+            return -h[-1]
+        
+# time: O(n * log n)
+# space: O(n)
