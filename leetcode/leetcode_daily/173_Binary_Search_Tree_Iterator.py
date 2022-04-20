650373574d5ce14a19f219224c36347c481bbc94
Nikhil <nik.dhoot@gmail.com>
2022-04-20 13:43:55 +0530

    binary search tree iterator

---
+++ leetcode/leetcode_daily/173_Binary_Search_Tree_Iterator.py
@@ -0,0 +1,85 @@
+'''
+173. Binary Search Tree Iterator
+
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+Example 1:
+Input
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 105].
+0 <= Node.val <= 106
+At most 105 calls will be made to hasNext, and next.
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+        self.inorder = []
+        def inorder(root):
+            if not root:
+                return root
+            
+            if root.left:
+                inorder(root.left)
+                
+            self.inorder.append(root.val)
+            
+            if root.right:
+                inorder(root.right)
+                
+            return
+        inorder(root)
+        self.itr = 0
+        
+
+    def next(self) -> int:
+        if self.itr < len(self.inorder):
+            returnVal = self.inorder[self.itr]
+            self.itr += 1
+            return returnVal
+        return None
+            
+        
+        
+
+    def hasNext(self) -> bool:
+        return self.itr < len(self.inorder)
+        
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+# time O(H), height of the tree
+# space O(N), number of nodes
